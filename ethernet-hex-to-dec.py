64ceb8ee70e4678ecd55732994bfc3c92c397110
district987 <45963946+district987@users.noreply.github.com>
2021-07-24 14:10:14 +0200

    Update ethernet-hex-to-dec.py

--- ethernet-hex-to-dec.py
+++ ethernet-hex-to-dec.py
@@ -1,12 +1,15 @@
-string = 'AAAAAAAAAAAAAAAB48057F9F8C53DD64FA4D4180080045000020975000002D1190DA5C0BF4BB95AACFB905346F96000C4B664348495000000000000000000000000000008C03CC85'
+string = 'AAAAAAAAAAAAAAAB823039C4030EB4E6D77A6F1208004500002513EA000027117C061B3CA0BC1D07051345BEF658001199E950524F5A4553534F52000000000000000000CEB8362F'
 splited = ""
 while string != "":
     splited += string[:2] + " "
     string = string[2:]
-print(splited)
 splited= splited.split()
+new = []
 
-final = ""
 for x in splited:
-    final += str(int(x,16)) + " "
-print(final)
+    new.insert(-1,int(x,16))
+new.insert(0,new.pop(-1))
+
+final = zip(splited,new)
+for x in final:
+    print(x)
